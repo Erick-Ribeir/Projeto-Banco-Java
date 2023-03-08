da4f799adacfa6639140d798e53f7cb4f671c0d0
Erick Wildson Ribeiro <102867844+Erick-Ribeir@users.noreply.github.com>
2023-03-07 21:21:32 -0300

    Add files via upload

---
+++ ProjetoBanco/src/projetobanco/Funcionario.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetobanco;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author 11067060642
+ */
+public class Funcionario {
+    private String nome;
+    private String departamento;
+    private LocalDate admissao;
+    private String rg;
+    private Boolean situacao;
+    private double salario; 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public LocalDate getAdmissao() {
+        return admissao;
+    }
+
+    public void setAdmissao(LocalDate admissao) {
+        this.admissao = admissao;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public Boolean getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(Boolean situacao) {
+        this.situacao = situacao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+    
+    }
+
